f105c2a81bc0449a6eb9bddac99e6d9178836a6b
vitek.lacko <vitek.lacko@VT-7>
2022-02-28 13:25:25 +0100

    Přidejte soubory projektu.

---
+++ bruh/Program.cs
@@ -0,0 +1,331 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace bruh
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int vek;
+            //string input = Console.ReadLine();
+            //vek = Convert.ToInt32(input);
+            //if (vek >= 0 && vek <= 1) {
+            //	Console.WriteLine("Kojenec");
+            //}
+            //else if (vek >= 1 && vek < 3) {
+            //	Console.WriteLine("Batole");
+            //}
+            //else if (vek >= 3 && vek < 6) {
+            //	Console.WriteLine("Předškolní věk");
+            //}
+            //else if (vek >= 6 && vek < 15) {
+            //	Console.WriteLine("Školák mladiství");
+            //}
+            //else if (vek >= 15 && vek < 18) {
+            //	Console.WriteLine("Mladistvý");
+            //}
+            //else if (vek >= 18 && vek < 65) {
+            //	Console.WriteLine("dospělý");
+            //}
+            //else if (vek >= 65 && vek < 120) {
+            //	Console.WriteLine("Senior");
+            //}
+            //else if (vek > 120) {
+            //	Console.WriteLine("Mrtvý");
+            //}
+
+
+            //string zemeEU = Console.ReadLine();
+            //switch (zemeEU)
+            //{
+            //	case "CZ":
+            //		{
+            //			Console.WriteLine("Česká Republika");
+            //			break;
+            //		}
+            //	case "SK":
+            //		{
+            //			Console.WriteLine("Slovesnká Republika");
+            //			break;
+            //		}
+            //	default:{
+            //			Console.WriteLine("Země není v EU");
+            //			break;
+            //		}
+            //	case "DE":
+            //		{
+            //			Console.WriteLine("Německo");
+            //			break;
+            //		}
+            //	case "F":
+            //		{
+            //			Console.WriteLine("Francie");
+            //			break;
+            //		}
+            //	case "PL":
+            //		{
+            //			Console.WriteLine("Polsko");
+            //			Console.WriteLine("Jsou tam lidi a mlubí polsky.");
+            //			break;
+            //		}
+
+
+
+
+            //megabruhh
+
+
+
+
+
+
+            //Console.WriteLine("Zadej řadu grafiky RTX: 2000/3000");
+            //int gang;
+            //string input = Console.ReadLine();
+            //gang = Convert.ToInt32(input);
+            //string stredni= Console.ReadLine(); ;
+            //if (gang == 2000){
+            //	Console.WriteLine("Chcete zobrazit střední nebo vyšší třídu?");
+            //	switch (stredni)
+            //	{
+            //		case "Střední":
+            //			Console.WriteLine("RTX 2060");
+            //			Console.WriteLine("RTX 2060SUPER");
+            //			Console.WriteLine("RTX 2070");
+            //			break;
+            //	}
+
+            //}
+            //else if (gang == 3000)
+            //{
+            //	Console.WriteLine("Chcete zobrazit střední nebo vyšší třídu?");
+            //}
+
+
+            //Console.WriteLine("Zadej řadu grafiky RTX: 2000/3000");
+            //int gang;
+            //string input = Console.ReadLine();
+            //gang = Convert.ToInt32(input);
+            //if (gang == 2000)
+            //{
+            //	Console.WriteLine("střední/vyšší");
+            //	string tridaGrafiky = Console.ReadLine();
+            //	if (tridaGrafiky == "střední")
+            //	{
+            //		Console.WriteLine("RTX2060,RTX2060S,RTX2080Ti");
+            //	}
+            //	else if (tridaGrafiky == "vyšší")
+            //	{
+            //		Console.WriteLine("RTX2070S,RTX2080,RTX2080Ti");
+            //	}
+            //}
+            //else if (gang == 3000)
+            //{
+            //	Console.WriteLine("Chcete zobrazit střední nebo vyšší třídu?");
+            //	string tridaGrafiky = Console.ReadLine();
+            //	if (tridaGrafiky == "střední")
+            //	{
+            //		Console.WriteLine("RTX3800,RTX2060S,RTX2080Ti");
+            //	}
+            //	else if (tridaGrafiky == "vyšší")
+            //	{
+            //		Console.WriteLine("RTX2070S,RTX2080,RTX2080Ti");
+            //	}
+            //}
+
+            //-------------------------------------------------------------------------------------------------------
+
+            //int[] obyvatele = new int[7];
+            //obyvatele[2] = 13;
+            //obyvatele[5] = 5;
+            //Console.WriteLine(obyvatele[2]);
+            //Console.WriteLine(obyvatele[5]);
+
+            //char[] geg = new char[8];
+
+            //string[] chabr = new string[4];
+
+            //chabr[0] = Console.ReadLine();
+            //chabr[1] = Console.ReadLine();
+            //chabr[2] = Console.ReadLine();
+            //chabr[3] = Console.ReadLine();
+            //Console.WriteLine(chabr[0]);
+            //Console.WriteLine(chabr[1]);
+            //Console.WriteLine(chabr[2]);
+            //Console.WriteLine(chabr[3]);
+
+            //string nazevMesta = "Žatec";
+            //nazevMesta = nazevMesta.ToUpper();
+            //Console.WriteLine(nazevMesta);
+
+            //string gang = "Žatec";
+            //gang = gang.ToLower();
+            //Console.WriteLine(gang);
+
+            //string veta = "chabr je chabr gang";
+            //bool obsahuje = veta.Contains("chabr");
+
+
+
+            //if (obsahuje == true)
+            //{
+            //	Console.ForegroundColor = ConsoleColor.Green;
+            //	Console.Write("zelenej chabr");
+            //}
+            //else
+            //{
+            //	Console.ForegroundColor = ConsoleColor.Red;
+            //	Console.Write("cervenej chabr");
+            //}
+
+
+
+
+            //string textInput1 = Console.ReadLine();
+            //string textInput2 = Console.ReadLine();
+            //string textInput3 = Console.ReadLine();
+
+            //int cislo1 = Convert.ToInt32(textInput1);
+            //int cislo2= Convert.ToInt32(textInput2);
+            //int cislo3 = Convert.ToInt32(textInput3);
+
+            //int vysledek = cislo1 + cislo2 + cislo3;
+            //int vysledek2 = cislo1 * cislo2* cislo3;
+
+            //Console.WriteLine(vysledek);
+            //Console.WriteLine(vysledek2);
+
+            //Console.WriteLine("Zadej výšku v metrech");
+            //string textInput1 = Console.ReadLine();
+            //Console.WriteLine("Zadej váhu v kg");
+            //string textInput2 = Console.ReadLine();
+
+
+
+            //int vysledek1 = cislo2 / cislo1 * cislo1;
+
+
+            //if (vysledek1 >= 0 && vysledek1 <= 18)
+            //{
+            //	Console.WriteLine("podváha");
+            //}
+            //else if(vysledek1 >= 18 && vysledek1 <= 25)
+            //{
+            //	Console.WriteLine("normální hmotnost");
+            //}
+            //else if (vysledek1 >= 25 && vysledek1 <= 30)
+            //{
+            //	Console.WriteLine("nadváha");
+            //}
+            //else if (vysledek1 >= 30)
+            //{
+            //	Console.WriteLine("obezita");
+            ////}
+
+            //string veta = "            olooooooooooooooooooooooo     bruh             ";
+            //Console.WriteLine(veta);
+            //Console.WriteLine(veta.TrimStart());
+            //Console.WriteLine(veta.TrimEnd());
+            //Console.WriteLine(veta.Trim());
+
+
+            //Console.WriteLine("Zadej jmeno: ");
+            //string jmeno = Console.ReadLine();
+            //Console.WriteLine(jmeno.Trim());
+            //Console.ReadLine();
+
+
+            //Console.WriteLine("Zadej jmeno: ");
+            //string jmeno = Console.ReadLine();
+            //Console.WriteLine("Zadej prijmeniii: ");
+            //string prijmeni = Console.ReadLine();
+            //jmeno = jmeno.Trim();
+            //prijmeni = prijmeni.Trim();
+            //string nazevJmenoXD = "Jste" + jmeno + " "+  prijmeni;
+
+
+            ////////////Console.WriteLine("Zadej výšku: ");
+            ////////////string cislak = Console.ReadLine();
+            ////////////int cislo = Convert.ToInt32(cislak);
+            ////////////Console.WriteLine("Zadej more cm nebo dm nebo m idk co chceš chapes ne proste metry, centiky nebo more decimetry ja nevim more co to znamena tohle je nejake bruh ay ay ay: ");
+            ////////////string dmcmm = Console.ReadLine();
+
+
+            //string vyskaInput = Console.ReadLine();
+            //double vyskaCislo = Convert.ToDouble(vyskaInput);
+            //Console.WriteLine("Zadej yuh uh yeah cm/dm/m uh uh: ");
+            //string jednotka = Console.ReadLine();
+
+            //switch (jednotka)
+            //{
+            //	case "cm":
+            //		Console.WriteLine(vyskaCislo * 10 + "mm");
+            //		break;
+            //	case "dm":
+            //		Console.WriteLine(vyskaCislo * 100 + "mm");
+            //	case "m":
+            //		Console.WriteLine(vyskaCislo * 1000 + "mm");
+            //		break;
+
+
+            //}
+
+            ////string g = "gang";
+            ////int jebe = g.Length;
+            ////Console.WriteLine(jebe);
+
+
+            //float cisloPi = Math.PI;
+            //float cisloE = Math.E;
+
+
+            //Console.WriteLine("Cislo more ");
+            //string cislomore = Console.ReadLine();
+            //Console.WriteLine("Cislo more dva ");
+            //string cislomoredva = Console.ReadLine();
+
+            //int[] poleCisla = { 5, 9, 14, 1, 20 };
+            //foreach (int cislo in poleCisla)
+            //{
+            //    if ((cislo % 2) == 0)
+            //    {
+            //        Console.WriteLine(cislo);
+            //        Console.ReadLine();
+            //    }
+            //}
+
+
+            //string oddelovanyText = "Martin, Lukáš, Anežka, Barbora";
+            //string[] oddeleny = oddelovanyText.Split(" , ");
+
+            //Console.WriteLine("Načti: ");
+            //string ahoj = Console.ReadLine();
+            //string[] oddeleny = ahoj.Split(" ; ");
+            //Array.Reverse(oddeleny);
+            //foreach (string frajer in oddeleny)
+            //{
+            //    Console.Write(frajer);
+            //}
+
+            //List<string> seznam = new List<string>();
+            //seznam.Add("KFC");
+            //seznam.Add("KFC");
+            //seznam.Add("Bugr King");
+            //foreach (string uzivatel in seznam)
+            //{
+            //    Console.WriteLine(uzivatel);
+            //}
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("cummin");
+            for (int i = 0; i <= 100; i = i + 5)
+            {
+                Console.WriteLine($"progress: {i}%");
+                Thread.Sleep(2000);
+            }
+            Console.WriteLine("cum successful");
+        }
+    }
+}
